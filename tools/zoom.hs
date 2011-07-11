eebc83d36c0cfc5767b4e73a05ce5c3f82470198
Conrad Parker <conrad@metadecks.org>
2011-07-11 13:04:56 +0900

    tool: add "gen" subcommand

--- tools/zoom.hs
+++ tools/zoom.hs
@@ -9,15 +9,18 @@ import UI.Command
 
 ------------------------------------------------------------
 
-zoomHello = defCmd {
-          cmdName = "hello"
-        , cmdHandler = zoomHelloHandler
-        , cmdCategory = "Reporting"
-        , cmdShortDesc = "Say Hello"
+zoomGen = defCmd {
+          cmdName = "gen"
+        , cmdHandler = zoomGenHandler
+        , cmdCategory = "Writing"
+        , cmdShortDesc = "Generate zoom data"
         , cmdExamples = [("Yo", "")]
         }
 
-zoomHelloHandler = liftIO . putStrLn $ "Hello world!"
+zoomGenHandler = liftIO $ mapM_ (putStrLn . show) =<< zoomGenDouble
+
+zoomGenDouble :: IO [Double]
+zoomGenDouble = return [3.5, 3.5, 4.2, 3.7, 3.6, 6.3, 6.7, 7.7, 4.3, 9.3]
 
 ------------------------------------------------------------
 -- The Application
@@ -31,10 +34,10 @@ zoom = def {
         , appBugEmail = "conrad@metadecks.org"
         , appShortDesc = "Trivial zoom inspection tools"
         , appLongDesc = longDesc
-        , appCategories = ["Reporting"]
+        , appCategories = ["Reporting", "Writing"]
         , appSeeAlso = [""]
         , appProject = "Zoom"
-        , appCmds = [zoomHello]
+        , appCmds = [zoomGen]
 	}
 
 longDesc = "This is a bunch of trivial routines for inspecting git repositories. It is in no way useful beyond that."
