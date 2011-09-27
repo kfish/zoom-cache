54f6004cbc6d2f68d3c3617c4a854b5df4158e72
Conrad Parker <conrad@metadecks.org>
2011-09-27 09:49:23 +0900

    Add FloatMinMax.hs by Johan Kiviniemi
    https://gist.github.com/1188115

---
+++ Zoom/FloatMinMax.hs
@@ -0,0 +1,38 @@
+-- | The minimum and maximum positive, finite floats.
+
+module FloatMinMax
+( floatMin
+, floatMaxDenorm
+, floatMinNorm
+, floatMax
+)
+where
+
+import Control.Applicative
+import Data.Function
+
+-- | The minimum positive, denormalized float.
+floatMin :: RealFloat a => a
+floatMin = fix $ do d     <- floatDigits
+                    (e,_) <- floatRange
+                    pure $ encodeFloat 1 (e-d)
+{-# INLINE floatMin #-}
+
+-- | The maximum denormalized float.
+floatMaxDenorm :: RealFloat a => a
+floatMaxDenorm = floatMinNorm - floatMin
+{-# INLINE floatMaxDenorm #-}
+
+-- | The minimum positive, normalized float.
+floatMinNorm :: RealFloat a => a
+floatMinNorm = fix $ do (e,_) <- floatRange
+                        pure $ encodeFloat 1 (e-1)
+{-# INLINE floatMinNorm #-}
+
+-- | The maximum finite float.
+floatMax :: RealFloat a => a
+floatMax = fix $ do r     <- floatRadix
+                    d     <- floatDigits
+                    (_,e) <- floatRange
+                    pure $ encodeFloat (r^d-1) (e-d)
+{-# INLINE floatMax #-}
