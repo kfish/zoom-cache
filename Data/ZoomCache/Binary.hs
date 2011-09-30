e2c5c4b4434b2afb3d61e8f7390dac95af936fc3
Conrad Parker <conrad@metadecks.org>
2011-09-30 12:38:20 +0900

    Binary: remove unused buildInt{16,32,64}
    The writing code uses fromInt{16,32,64}be builders directly now

--- Data/ZoomCache/Binary.hs
+++ Data/ZoomCache/Binary.hs
@@ -3,17 +3,15 @@
 {-# OPTIONS -Wall #-}
 
 module Data.ZoomCache.Binary (
-      buildInt16
-    , buildInt32
-    , buildInt64
-    , encInt
-    , encDbl
-    , toWord64
-
     -- * Builders
+      encInt
+    , encDbl
     , fromTrackType
     , fromTrackNo
     , fromSummary
+
+    -- * Helpers
+    , toWord64
 ) where
 
 import Blaze.ByteString.Builder hiding (flush)
@@ -72,15 +70,6 @@ fromSummaryHeader s = mconcat
 ----------------------------------------------------------------------
 -- Binary data helpers
     
-buildInt16 :: Int -> L.ByteString
-buildInt16 = toLazyByteString . fromInt16be . fromIntegral
-
-buildInt32 :: Int -> L.ByteString
-buildInt32 = toLazyByteString . fromInt32be . fromIntegral
-
-buildInt64 :: Integer -> L.ByteString
-buildInt64 = toLazyByteString . fromInt64be . fromIntegral
-
 encInt :: forall a . (Integral a) => a -> Builder
 encInt = fromInt32be . fromIntegral
 
