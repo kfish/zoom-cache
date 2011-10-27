7f830f1a1e7cbdb216af51dcb397155605dcd7aa
Conrad Parker <conrad@metadecks.org>
2011-10-27 16:34:34 +0800

    Iteratee.ZC.Utils: add haddocks

--- Data/Iteratee/ZoomCache/Utils.hs
+++ Data/Iteratee/ZoomCache/Utils.hs
@@ -30,29 +30,35 @@ import Data.Ratio
 import Data.Word
 import Unsafe.Coerce (unsafeCoerce)
 
+-- | Read 2 bytes as a big-endian Int.
 zReadInt16 :: (Functor m, MonadIO m) => Iteratee [Word8] m Int
 zReadInt16 = fromIntegral . u16_to_s16 <$> I.endianRead2 I.MSB
     where
         u16_to_s16 :: Word16 -> Int16
         u16_to_s16 = fromIntegral
 
+-- | Read 4 bytes as a big-endian Int.
 zReadInt32 :: (Functor m, MonadIO m) => Iteratee [Word8] m Int
 zReadInt32 = fromIntegral . u32_to_s32 <$> I.endianRead4 I.MSB
     where
         u32_to_s32 :: Word32 -> Int32
         u32_to_s32 = fromIntegral
 
+-- | Read 8 bytes as a big-endian Integer
 zReadInt64 :: (Functor m, MonadIO m) => Iteratee [Word8] m Integer
 zReadInt64 = fromIntegral . u64_to_s64 <$> I.endianRead8 I.MSB
     where
         u64_to_s64 :: Word64 -> Int64
         u64_to_s64 = fromIntegral
 
+-- | Read 8 bytes as a big-endian Double
 zReadFloat64be :: (Functor m, MonadIO m) => Iteratee [Word8] m Double
 zReadFloat64be = do
     n <- I.endianRead8 I.MSB
     return (unsafeCoerce n :: Double)
 
+-- | Read 16 bytes as a big-endian Rational, encoded as an 8 byte
+-- big endian numerator followed by an 8 byte big endian denominator.
 readRational64 :: (Functor m, MonadIO m) => Iteratee [Word8] m Rational
 readRational64 = do
     num <- zReadInt64
