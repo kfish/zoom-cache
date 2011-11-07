9ab482720cf69e2d6d0c008e433f86797af2cf36
Conrad Parker <conrad@metadecks.org>
2011-11-07 16:36:54 +0800

    add D.Z.Numeric.{Internal,Types}

---
+++ Data/ZoomCache/Numeric/Internal.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS -Wall #-}
+
+module Data.ZoomCache.Numeric.Internal (
+    -- * Functions
+      readSummaryNum
+    , fromSummaryNum
+    , initSummaryNumBounded
+    , mkSummaryNum
+    , appendSummaryNum
+    , updateSummaryNum
+) where
+
+import Blaze.ByteString.Builder
+import Control.Monad (replicateM)
+import Control.Monad.Trans (MonadIO)
+import Data.Iteratee (Iteratee)
+import qualified Data.Iteratee as I
+import qualified Data.ListLike as LL
+import Data.Maybe (fromMaybe)
+import Data.Monoid
+import Data.Word
+
+import Data.ZoomCache.Codec
+import Data.ZoomCache.Numeric.Types
+
+----------------------------------------------------------------------
+
+readSummaryNum :: (I.Nullable s, LL.ListLike s Word8,
+                   Functor m, MonadIO m,
+                   ZoomNum a)
+               => Iteratee s m (SummaryData a)
+readSummaryNum = do
+    [en,ex,mn,mx] <- replicateM 4 readRaw
+    [avg,rms] <- replicateM 2 readDouble64be
+    return (numMkSummary en ex mn mx avg rms)
+{-# INLINABLE readSummaryNum #-}
+
+fromSummaryNum :: ZoomNum a
+               => SummaryData a -> Builder
+fromSummaryNum s = mconcat $
+    map numFromRaw [numMin s, numMax s] ++
+    map fromDouble [numAvg s, numRMS s]
+{-# INLINABLE fromSummaryNum #-}
+
+initSummaryNumBounded :: (Bounded a, ZoomNum a)
+                      => TimeStamp -> SummaryWork a
+initSummaryNumBounded entry = numMkSummaryWork entry Nothing 0 maxBound minBound 0.0 0.0
+{-# INLINEABLE initSummaryNumBounded #-}
+
+mkSummaryNum :: ZoomNum a
+             => TimeStampDiff -> SummaryWork a
+             -> SummaryData a
+mkSummaryNum (TSDiff dur) sw =
+    numMkSummary (fromMaybe 0 $ numWorkEntry sw) (numWorkExit sw)
+                 (numWorkMin sw) (numWorkMax sw)
+                 (numWorkSum sw / fromIntegral dur)
+                 (sqrt $ (numWorkSumSq sw) / fromIntegral dur)
+{-# INLINEABLE mkSummaryNum #-}
+
+appendSummaryNum :: ZoomNum a
+                 => TimeStampDiff -> SummaryData a
+                 -> TimeStampDiff -> SummaryData a
+                 -> SummaryData a
+appendSummaryNum (TSDiff dur1) s1 (TSDiff dur2) s2 = numMkSummary
+    (numEntry s1)
+    (numExit s2)
+    (min (numMin s1) (numMin s2))
+    (max (numMax s1) (numMax s2))
+    (((numAvg s1 * fromIntegral dur1) + (numAvg s2 * fromIntegral dur2)) / fromIntegral durSum)
+    (sqrt $ ((numRMS s1 * numRMS s1 * fromIntegral dur1) +
+             (numRMS s2 * numRMS s2 * fromIntegral dur2)) /
+            fromIntegral durSum)
+    where
+        !durSum = dur1 + dur2
+{-# INLINEABLE appendSummaryNum #-}
+
+updateSummaryNum :: ZoomNum a
+                 => TimeStamp -> a
+                 -> SummaryWork a
+                 -> SummaryWork a
+updateSummaryNum t d sw =
+    numMkSummaryWork t (Just $ fromMaybe d (numWorkEntry sw))
+                       d
+                       (min (numWorkMin sw) d)
+                       (max (numWorkMax sw) d)
+                       ((numWorkSum sw) + realToFrac (d * dur))
+                       ((numWorkSumSq sw) + realToFrac (d*d * dur))
+    where
+        !dur = fromIntegral $ (unTS t) - (unTS (numWorkTime sw))
+{-# INLINEABLE updateSummaryNum #-}
---
+++ Data/ZoomCache/Numeric/Types.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS -Wall #-}
+
+module Data.ZoomCache.Numeric.Types (
+    -- * Classes
+      ZoomNum(..)
+) where
+
+import Blaze.ByteString.Builder
+import Data.ZoomCache.Codec
+
+----------------------------------------------------------------------
+-- ZoomNum
+
+class (Ord a, Real a, ZoomReadable a, ZoomWritable a) => ZoomNum a where
+    numFromRaw :: a -> Builder
+
+    numEntry :: SummaryData a -> a
+    numExit  :: SummaryData a -> a
+    numMin   :: SummaryData a -> a
+    numMax   :: SummaryData a -> a
+    numAvg   :: SummaryData a -> Double
+    numRMS   :: SummaryData a -> Double
+
+    numWorkTime  :: SummaryWork a -> TimeStamp
+    numWorkEntry :: SummaryWork a -> Maybe a
+    numWorkExit  :: SummaryWork a -> a
+    numWorkMin   :: SummaryWork a -> a
+    numWorkMax   :: SummaryWork a -> a
+    numWorkSum   :: SummaryWork a -> Double
+    numWorkSumSq :: SummaryWork a -> Double
+
+    numMkSummary :: a -> a -> a -> a -> Double -> Double -> SummaryData a
+    numMkSummaryWork :: TimeStamp -> Maybe a -> a -> a -> a -> Double -> Double
+                     -> SummaryWork a
+
