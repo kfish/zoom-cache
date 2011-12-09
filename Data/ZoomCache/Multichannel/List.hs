72708b56b59f42c70a2c1cbff5a03bbc627cec2e
Conrad Parker <conrad@metadecks.org>
2011-12-09 11:04:02 +0800

    Multichannel: add wholeTrackSummaryListDouble, enumSummaryListDouble

--- Data/ZoomCache/Multichannel/List.hs
+++ Data/ZoomCache/Multichannel/List.hs
@@ -23,13 +23,26 @@ for type a.
 module Data.ZoomCache.Multichannel.List (
       SummaryData(..)
     , SummaryWork(..)
+
+    , wholeTrackSummaryListDouble
+    , enumSummaryListDouble
 )where
 
+import Control.Applicative ((<$>))
+import Control.Monad.Trans (MonadIO)
+import Data.ByteString (ByteString)
+import Data.Int
+import qualified Data.Iteratee as I
+import Data.Maybe
+import Data.Typeable
 import Data.TypeLevel.Num hiding ((==))
+import Data.Word
 
+import Data.Iteratee.ZoomCache
 import Data.ZoomCache.Codec
 import Data.ZoomCache.Multichannel.NList
 import Data.ZoomCache.NList
+import Data.ZoomCache.Numeric
 
 ----------------------------------------------------------------------
 
@@ -44,3 +57,64 @@ instance (ZoomWrite a, ZoomWritable a) => ZoomWrite (SampleOffset, [a]) where
 
 writeSOList :: (ZoomWrite a, ZoomWritable a) => TrackNo -> (SampleOffset, [a]) -> ZoomW ()
 writeSOList tn (ts, xs) = reifyIntegral (length xs) (\n -> write tn (ts, (NList n xs)))
+
+----------------------------------------------------------------------
+
+toSummaryListDouble :: Typeable a => Summary a -> Maybe [Summary Double]
+toSummaryListDouble s | typeOf s == typeOf (undefined :: Summary (NList D1 Double)) =
+                                sl <$> (cast s :: Maybe (Summary (NList D1 Double)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Float)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Float)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Int)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Int)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Int8)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Int8)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Int16)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Int16)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Int32)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Int32)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Int64)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Int64)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Integer)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Integer)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Word)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Word)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Word8)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Word8)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Word16)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Word16)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Word32)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Word32)))
+                      | typeOf s == typeOf (undefined :: Summary (NList D1 Word64)) =
+                               sld <$> (cast s :: Maybe (Summary (NList D1 Word64)))
+                      | otherwise = Nothing
+    where
+        sl :: Summary (NList D1 a) -> [Summary a]
+        sl = summaryNListToList
+        sld :: Typeable a => Summary (NList D1 a) -> [Summary Double]
+        sld = catMaybes . map toSummaryDouble . sl
+
+----------------------------------------------------------------------
+
+-- | Read the summary of an entire track.
+wholeTrackSummaryListDouble :: (Functor m, MonadIO m)
+                            => [IdentifyCodec]
+                            -> TrackNo
+                            -> I.Iteratee ByteString m [Summary Double]
+wholeTrackSummaryListDouble identifiers trackNo =
+    I.joinI $ enumCacheFile identifiers .
+    I.joinI . filterTracks [trackNo] .  I.joinI . e $ I.last
+    where
+        e = I.joinI . enumSummaries . I.mapChunks (catMaybes . map toSLD)
+        toSLD :: ZoomSummary -> Maybe [Summary Double]
+        toSLD (ZoomSummary s) = toSummaryListDouble s
+
+enumSummaryListDouble :: (Functor m, MonadIO m)
+                      => Int
+                      -> I.Enumeratee [Stream] [[Summary Double]] m a
+enumSummaryListDouble level =
+    I.joinI . enumSummaryLevel level .
+    I.mapChunks (catMaybes . map toSLD)
+    where
+        toSLD :: ZoomSummary -> Maybe [Summary Double]
+        toSLD (ZoomSummary s) = toSummaryListDouble s
