e56a3166ddfbcd332b964a5eea25f8748587510b
Conrad Parker <conrad@metadecks.org>
2012-01-03 14:18:07 +0800

    add instance Nullable Data.ByteString.Offset

--- Data/ByteString/Offset.hs
+++ Data/ByteString/Offset.hs
@@ -6,12 +6,16 @@ module Data.ByteString.Offset (
 ) where
 
 import qualified Data.ByteString as BS
+import Data.Nullable
 import Data.NullPoint
 import System.Posix.Types (FileOffset)
 
 ----------------------------------------------------------------------
 
 data ByteString = OBS {-# UNPACK #-}!FileOffset {-# UNPACK #-}!BS.ByteString
+
+instance Nullable ByteString where
+    nullC (OBS _ bs) = nullC bs
 
 instance NullPoint ByteString where
     empty = OBS 0 empty
