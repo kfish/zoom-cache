7a6315d3433ef1ca4c1ad708a415aa265658ee59
Conrad Parker <conrad@metadecks.org>
2012-01-03 12:08:12 +0800

    D.B.Offset: switch to FileOffset, add NullPoint instance

--- Data/ByteString/Offset.hs
+++ Data/ByteString/Offset.hs
@@ -6,8 +6,12 @@ module Data.ByteString.Offset (
 ) where
 
 import qualified Data.ByteString as BS
-import Data.Int
+import Data.NullPoint
+import System.Posix.Types (FileOffset)
 
 ----------------------------------------------------------------------
 
-data ByteString = OBS {-# UNPACK #-}!Int64 {-# UNPACK #-}!BS.ByteString
+data ByteString = OBS {-# UNPACK #-}!FileOffset {-# UNPACK #-}!BS.ByteString
+
+instance NullPoint ByteString where
+    empty = OBS 0 empty
