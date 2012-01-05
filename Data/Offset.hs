7a61a8c170500d9cbc6657350ef134e2bee2a024
Conrad Parker <conrad@metadecks.org>
2012-01-05 10:20:26 +0800

    Data.Offset: add Monoid instance, unwrapOffset

--- Data/Offset.hs
+++ Data/Offset.hs
@@ -3,8 +3,12 @@
 module Data.Offset (
     -- * Types
       Offset(..)
+
+    -- * Functions
+    , unwrapOffset
 ) where
 
+import Data.Monoid
 import Data.Nullable
 import Data.NullPoint
 import System.Posix.Types (FileOffset)
@@ -18,3 +22,13 @@ instance Nullable a => Nullable (Offset a) where
 
 instance NullPoint a => NullPoint (Offset a) where
     empty = Offset 0 empty
+
+instance Monoid a => Monoid (Offset a) where
+    mempty = Offset 0 mempty
+    mappend (Offset o1 s1) (Offset _ s2) = Offset o1 (s1 `mappend` s2)
+    mconcat [] = mempty
+    mconcat [x] = x
+    mconcat ((Offset o x):xs) = Offset o (mconcat (x:(map unwrapOffset xs)))
+
+unwrapOffset :: Offset a -> a
+unwrapOffset (Offset _ x) = x
