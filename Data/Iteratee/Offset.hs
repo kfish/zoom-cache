f8d4739c26a89fef7310dc4a0ee5dbde412488da
Conrad Parker <conrad@metadecks.org>
2012-01-12 11:11:14 +0800

    D.I.Offset: add convOffset

--- Data/Iteratee/Offset.hs
+++ Data/Iteratee/Offset.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
 {-# OPTIONS -Wall #-}
 
 module Data.Iteratee.Offset (
       tell
+
+    -- * Iteratee conversion
+    , convOffset
 
     -- * ListLike
     , takeBS
@@ -35,6 +39,47 @@ tell = I.liftI step
       | LL.null vec = I.liftI step
       | otherwise   = I.idone o s
     step stream     = I.icont step (Just (I.setEOF stream))
+
+----------------------------------------------------------------------
+
+-- | Run a ByteString iteratee on an (Offset ByteString) input stream
+convOffset :: (Monad m)
+           => Iteratee ByteString m a
+           -> Iteratee (Offset ByteString) m a
+convOffset i = do
+    n <- liftM fromIntegral tell
+    go n (const i) (I.Chunk I.empty)
+  where
+    go :: (Monad m)
+       => Integer
+       -> (I.Stream ByteString -> Iteratee ByteString m a)
+       -> I.Stream (Offset ByteString)
+       -> Iteratee (Offset ByteString) m a
+    go !n f str@(I.EOF _) = I.Iteratee rI
+      where
+        rI od oc = I.runIter (f (unwrapStream str)) onDone onCont
+          where
+            onDone a _ = od a str
+            onCont f' mExc = oc (go n f') mExc
+    go !n f str@(I.Chunk c) = I.Iteratee rI
+      where
+        newLen = n + fromIntegral (LL.length c)
+        rI od oc = I.runIter (f (unwrapStream str)) onDone onCont
+          where
+            onDone a str'@(I.Chunk c') =
+                let n' = newLen - fromIntegral (LL.length c') in
+                od a (wrapStream n' str')
+            onDone a str'@(I.EOF _) = od a (wrapStream n str')
+            onCont f' mExc = oc (go newLen f') mExc
+{-# INLINE convOffset #-}
+
+unwrapStream :: I.Stream (Offset a) -> I.Stream a
+unwrapStream (I.EOF e) = I.EOF e
+unwrapStream (I.Chunk (Offset _ xs)) = I.Chunk xs
+
+wrapStream :: Integral n => n -> I.Stream a -> I.Stream (Offset a)
+wrapStream _ (I.EOF e) = I.EOF e
+wrapStream n (I.Chunk xs) = I.Chunk (Offset (fromIntegral n) xs)
 
 ----------------------------------------------------------------------
 
