018b9c84c1a133ee0d4f77bd80d2c839299f9ab7
Conrad Parker <conrad@metadecks.org>
2012-01-27 09:37:27 +0800

    NList: build fix for ghc < 7.2

--- Data/ZoomCache/NList.hs
+++ Data/ZoomCache/NList.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -Wall #-}
@@ -34,7 +35,11 @@ instance Eq a => Eq (NList n a) where
     (NList _ a1) == (NList _ a2) = a1 == a2
 
 instance Typeable a => Typeable (NList n a) where
+#if __GLASGOW_HASKELL__ >= 702
     typeOf _ = mkTyConApp (mkTyCon3 "zoom-cache" "Data.ZoomCache.NList" "NList") [typeOf (undefined :: a)]
+#else
+    typeOf _ = mkTyConApp (mkTyCon "Data.ZoomCache.NList") [typeOf (undefined :: a)]
+#endif
 
 instance (Nat n, Arbitrary a) => Arbitrary (NList n a) where
     arbitrary = NList unify <$> sequence [ arbitrary | _ <- [1..(toInt unify)] ]
